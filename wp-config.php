1c2c3f0f6cb31269b8f5b5ba35541ae90283c570
zulfikarkhalwaniev <fikarezpz@gmail.com>
2024-07-10 14:57:20 +0700

    Update and rename wp-config-sample.php to wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'db_wp' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'user_wp' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'trinix%55' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
